145e9b3079321494d9ca22453f653f7ab8ef24c0
TahirMansuri <tprocks786@gmail.com>
2022-03-29 17:47:04 +0530

    ADDED: Display Hello Message

---
+++ src/main/java/com/infogalaxy/myspringproject/MySpringProjectApplication.java
@@ -0,0 +1,14 @@
+package com.infogalaxy.myspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringProjectApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from InfoGalaxy");
+        SpringApplication.run(MySpringProjectApplication.class, args);
+    }
+
+}
